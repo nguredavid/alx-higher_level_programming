7a0a41fce033ed087d12c2f4c268577f452c2403
David <64853005+nguredavid@users.noreply.github.com>
2022-06-29 16:32:05 +0300

    Update 6-print_comb3.py
    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=', ')
-print("{:d}{:d}".format(i + 1, j))
+for a in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(a), end='')
