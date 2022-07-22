d04da7659dd580b43ef0d95e2f7781bf683255ac
David <64853005+nguredavid@users.noreply.github.com>
2022-07-22 08:37:58 +0300

    Update 1-square.py
    fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,11 +3,14 @@
 
 
 class Square:
+    
     """Simple square class with his size as a field"""
 
     def __init__(self, size):
+        
         """ Instance the class Square
             Arguments:
-                @size: the size of every side of the Square"""
+                @size: the size of every side of the Square
+         """
         
         self.__size = size #end
