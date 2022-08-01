cdaeae85bdf7583d7408a6463fb20b71aaf51393
David <64853005+nguredavid@users.noreply.github.com>
2022-08-01 20:42:24 +0300

    Update 8-rectangle.py
    fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,85 +1,44 @@
 #!/usr/bin/python3
-""" this module creates
-an empty rectangle
+"""
+   6-base_geometry.py module
+   Class BaseGeometry.
+   Public instance method: \
+   def area(self): that raises an Exception with\
+   the message area() is not implemented.
+   Public instance method:\
+   def integer_validator(self, name, value):\
+   that validates value
 """
 
 
-class Rectangle:
-    """this class defines a rectangle"""
-    number_of_instances = 0
-
-    def __init__(self, width=0, height=0):
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
-        Rectangle.number_of_instances = Rectangle.number_of_instances + 1
-
-    @property
-    def width(self):
-        """this instance method returns the width"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """this instance method sets the value of the width"""
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """this method returns the height field"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """this method sets he height field"""
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+class BaseGeometry:
+    """Class BaseGeometry"""
 
     def area(self):
-        """this method returns the area of the rectangle"""
-        return self.__width * self.__height
+        """Public instance method: def area(self): \
+           that raises an Exception with\
+           the message area() is not implemented."""
+        raise Exception("area() is not implemented")
 
-    def perimeter(self):
-        """returns the perimeter of the rectangle"""
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return (2 * self.__width) + (2 * self.__height)
+    def integer_validator(self, name, value):
+        """Public instance method:\
+           def integer_validator(self, name, value):\
+           that validates value"""
+        if type(value) != int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
 
-    def __str__(self):
-        """returns the str repres. of the rect."""
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        string = ""
-        for i in range(0, self.__height):
-            for j in range(0, self.__width):
-                string = string + "#"
-            if i != self.__height - 1:
-                string = string + "\n"
-        return string
 
-    def __repr__(self):
-        """returns a string repres. of the rect. so eval can recreate"""
-        string = "Rectangle("
-        string = string + str(self.__width) + ", " + str(self.height) + ")"
-        return string
+class Rectangle(BaseGeometry):
+    """Class Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """Instantiation with width and height: \
+        def __init__(self, width, height)
+        width and height must be positive integers,\
+        validated by integer_validator"""
 
-    def __del__(self):
-        """deletes an istance of this class"""
-        if Rectangle.number_of_instances != 0:
-            Rectangle.number_of_instances = Rectangle.number_of_instances - 1
-        print("Bye rectangle...")
-        del self
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = 
