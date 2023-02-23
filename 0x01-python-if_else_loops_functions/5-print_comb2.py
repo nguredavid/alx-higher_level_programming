3a32262cbdc0af5eb65d472e8181ade9237d5f9d
daviengush@gmail.com <daviengush@gmail.com>
2023-02-23 12:55:51 +0000

    fixed

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
 #A program that prints numbers from 0 to 99
-
-def numbers():
-    for i in range(0, 100):
-        if i == 99:
-            print(f'{i:02}')
-            continue
-        print(f'{i:02}, ', end="")
-        
-numbers()
+for i in range(0, 100):
+    if i == 99:
+        print(f'{i:02}')
+        continue
+    print(f'{i:02}, ', end="")
