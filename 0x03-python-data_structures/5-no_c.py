1f7121e7bc2050b1fc7422e088c2ef74b2ca82fb
daviengush@gmail.com <daviengush@gmail.com>
2023-03-07 04:48:54 +0000

    fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new += i
+    for char in my_string:
+        if char is not ['c', 'C']:
+            new += char
     return new
