ab3cf88cafb554232450072192ac06da6c29fb6d
daviengush@gmail.com <daviengush@gmail.com>
2023-07-12 05:05:59 +0000

    fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-'''Write a function that reads a text file (UTF8) and prints it to stdout:'''
+"""Write a function that reads a text file (UTF8) and prints it to stdout:"""
 
 def read_file(filename=""):
 
-    '''This function reads the file content'''
+    """This function reads the file content"""
 
 
     with open(filename, "r", encoding="UTF-8") as myfile:
